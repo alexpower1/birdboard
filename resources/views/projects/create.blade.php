13f00faf904797e199ef0b65230ffc59dd19a96a
alexpower1 <alex@bepowerful.co.uk>
2019-09-09 19:23:29 +0100

    Store changes

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Birdboard</title>
+        
+    </head>
+    <body>
+        <h1>Create a Project</h1>
+
+        <form>
+            <div class="field">
+                <label for="title" class="label">Title</label>
+
+                <div class="control">
+                    <input type="text" class="input" name="title" placeholder="Title">
+                </div>
+            </div>
+
+            <div class="field">
+                <label for="description" class="label">Description</label>
+
+                <div class="control">
+                    <input type="text" class="input" name="description" placeholder="Description">
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <button type="submit" class="button is-link">Create Project</button>
+                </div>
+            </div>
+        </form>
+    </body>
+</html>
