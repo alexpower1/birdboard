e76ba8203f864676ec03ab41027b0e98c96e5f95
alexpower1 <alex@bepowerful.co.uk>
2019-09-13 19:20:09 +0100

    Added tailwind

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,36 +1,32 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Birdboard</title>
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
-    </head>
-    <body style="padding: 30px">
-        <h1 class="heading is-1">Create a project<h1>
-        
-            <form method="POST" action="/projects" class="container" style="padding-top: 40px">
-                @csrf
+@extends('layouts.app')
 
-                <div class="field">
-                    <label class="label" for="title">Title</label>
+@section('content')
+    <h1 class="heading is-1">Create a project</h1>
 
-                    <div class="control">
-                        <input type="text" class="input" name="title" placeholder ="Title">
-                    </div>
-                </div>
+    <form method="POST" action="/projects">
+        @csrf
 
-                <div class="field">
-                    <label class="label" for="description">Description</label>
+        <div class="field">
+            <label class="label" for="title">Title</label>
 
-                    <div class="control">
-                        <textarea name="description" class="textarea"></textarea>
-                    </div>
-                </div>
+            <div class="control">
+                <input type="text" class="input" name="title" placeholder ="Title">
+            </div>
+        </div>
 
-                <div class="field">
-                    <div class="control">
-                        <button type="submit" class="button is-link">Create Project</button>
-                    </div>
-                </div>
-            </form>
-    </body>
-</html>
+        <div class="field">
+            <label class="label" for="description">Description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea"></textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Create Project</button>
+                <a href="/projects">Cancel</a>
+            </div>
+        </div>
+    </form>
+@endsection
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Birdboard</h1>
-    <ul>
+    <header class="flex items-centered mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-gray-600 font-sm font-normal">My Projects</h2>
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
+
+    <main class="lg:flex flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <li>
-                    <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white rounded-lg shadow p-5" style="height: 250px;">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-200 pl-4">
+                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                    </h3>
+    
+                    <div class="text-gray-600">{{ str_limit($project->description, 100) }}</div>
+                </div>
+            </div>
         @empty
-            <li>No projects yet</li>
+            <div>No projects yet.</div>
         @endforelse
-    </ul>
+    </main>
 @endsection
