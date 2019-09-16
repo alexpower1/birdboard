d9de3a7f327f47efd3b79e9a71112e42324909ec
alexpower1 <alex@bepowerful.co.uk>
2019-09-16 21:01:11 +0100

    Task UI updates

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -16,22 +16,21 @@
 
     <main>
         <div class="lg:flex -mx-3">
-            <div class="lg:w-3/4 px-3 mb-6 lg:mb-0">
+            <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
                     <h2 class="text-gray-600 text-lg font-normal mb-3">Tasks</h2>
-                    {{-- <div class="card mb-3">Lorem ipsum</div>
-                    <div class="card mb-3">Lorem ipsum</div>
-                    <div class="card mb-3">Lorem ipsum</div>
-                    <div class="card mb-3">Lorem ipsum</div>
-                    <div class="card">Lorem ipsum</div> --}}
 
-                    @forelse ($project->tasks as $task)
-                        <div class="lg:w-1/3 px-3 pb-6">
-                            @include('tasks.card')
-                        </div>
-                    @empty
-                        <div>No tasks yet.</div>
-                    @endforelse
+                    @foreach ($project->tasks as $task)
+                        <div class="card mb-3">{{ $task->body }}</div>
+                    @endforeach
+
+                    <div class="card mb-3">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
+                            @csrf
+
+                            <input class="w-full" name="body" placeholder="Add a new task...">
+                        </form>
+                    </div>
                 </div>
 
                 <div class="mb-8">
@@ -39,7 +38,7 @@
                     <textarea class="card w-full" style="min-height: 200px">Lorem ipsum</textarea>
                 </div>
             </div>
-            <div class="lg:w-1/4 px-3">
+            <div class="lg:w-1/4 px-3 lg:py-10">
                 @include ('projects.card')
             </div>
         </div>
--- resources/views/tasks/card.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="card" style="height: 250px;">
-    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-200 pl-4">
-        {{ $task->title }}
-    </h3>
-
-    <div class="text-gray-600">
-        {{ $task->body }}
-    </div>
-</div>
