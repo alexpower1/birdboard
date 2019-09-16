7f04cabdbfbdb9f3f10c1781b08993f3724f3c33
alexpower1 <alex@bepowerful.co.uk>
2019-09-16 19:42:28 +0100

    Tasks testing begun

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -11,13 +11,7 @@
     <main class="lg:flex flex-wrap -mx-3">
         @forelse ($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white rounded-lg shadow p-5" style="height: 250px;">
-                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-200 pl-4">
-                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                    </h3>
-    
-                    <div class="text-gray-600">{{ str_limit($project->description, 100) }}</div>
-                </div>
+                @include('projects.card')
             </div>
         @empty
             <div>No projects yet.</div>
